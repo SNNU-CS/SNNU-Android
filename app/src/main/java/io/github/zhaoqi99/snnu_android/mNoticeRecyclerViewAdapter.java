69c18b6fc6d41b73e506403cfb27238b28e4b4e5
Qi Zhao <zhaoqi99@outlook.com>
2018-12-22 19:18:24 +0800

    add CardView in recyclerview

--- app/src/main/java/io/github/zhaoqi99/snnu_android/mNoticeRecyclerViewAdapter.java
+++ app/src/main/java/io/github/zhaoqi99/snnu_android/mNoticeRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package io.github.zhaoqi99.snnu_android;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.view.ViewGroup;
 
 import java.util.List;
+
+import io.github.zhaoqi99.snnu_android.Model.NoticeMessage;
 
 public class mNoticeRecyclerViewAdapter extends RecyclerView.Adapter<mNoticeRecyclerViewAdapter.ViewHolder>{
     private List<NoticeMessage> messageList;
@@ -23,9 +26,11 @@ public class mNoticeRecyclerViewAdapter extends RecyclerView.Adapter<mNoticeRecy
         TextView Title;
         TextView Date;
         TextView Department;
+        CardView CardView;
         public ViewHolder(View view)
         {
             super(view);
+            CardView=(CardView)view.findViewById(R.id.card_view);
             Title = (TextView)view.findViewById(R.id.txt_Title);
             Date = (TextView)view.findViewById(R.id.txt_Date);
             Department = (TextView)view.findViewById(R.id.txt_Department);
@@ -48,7 +53,7 @@ public class mNoticeRecyclerViewAdapter extends RecyclerView.Adapter<mNoticeRecy
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notice_recycler_layout,parent,false);
         final ViewHolder holder = new ViewHolder(view);
-        holder.Title.setOnClickListener(new View.OnClickListener() {
+        holder.CardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int position=holder.getAdapterPosition();
@@ -60,6 +65,18 @@ public class mNoticeRecyclerViewAdapter extends RecyclerView.Adapter<mNoticeRecy
                 v.getContext().startActivity(intent);
             }
         });
+//        holder.Title.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int position=holder.getAdapterPosition();
+//                String url=messageList.get(position).GetLink();
+//                Uri uri=Uri.parse(url);
+//                Intent intent=new Intent();
+//                intent.setAction(Intent.ACTION_VIEW);
+//                intent.setData(uri);
+//                v.getContext().startActivity(intent);
+//            }
+//        });
         return holder;
     }
 }
